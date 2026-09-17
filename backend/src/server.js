7b53ae4e63fcbc43ea60a50ae908ac7f4c7217b0
shivraj2328 <shivrajjagtap6@gmail.com>
2026-09-17 10:18:09 +0530

    build: imported errorHandler

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,32 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require('cors');
-const logger = require('./utils/logger');
-const v1Routes = require('./routes/v1');
+
+const { logger } = require('./utils/logger'); 
+
+// Importing custom error handler middleware
+const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 //middlewares
 app.use(cors());
 app.use(express.json());
-
-app.use((req, res, next) => {
-  logger.info({ method: req.method, url: req.originalUrl }, 'Incoming request');
-  next();
-});
+app.use(express.urlencoded({extended:true}));
 
 //versioning
+const v1Routes = require('./routes/v1');
 app.use('/api/v1', v1Routes);
 
-app.use((err, req, res, next) => {
-  logger.error({ err, method: req.method, url: req.originalUrl }, 'Unhandled error');
-  res.status(500).json({ message: 'Internal Server Error' });
+// Root endpoint / Health check
+app.get("/",(req,res)=>{
+    res.status(200).json({status:"success",message:"TraceGuard API is running "});
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  logger.info(`Server running at http://localhost:${PORT}`);
-});
+app.use(errorHandler);
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
